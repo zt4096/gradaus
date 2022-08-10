dcc944d534d23833f5775263fee57f9f75a5a42d
zt4096 <52754492+zt4096@users.noreply.github.com>
2022-08-10 15:20:13 +0500

    fixed.

--- gradaus.py
+++ gradaus.py
@@ -4,46 +4,47 @@ import os
 from datetime import datetime
 
 headers = {
-    'Connection': 'keep-alive',
-    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
-    'accept': '*/*',
-    'content-type': 'text/plain',
-    'sec-ch-ua-mobile': '?0',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
-    'sec-ch-ua-platform': '"Windows"',
-    'Origin': 'https://gradaustralia.com.au',
-    'Sec-Fetch-Site': 'cross-site',
-    'Sec-Fetch-Mode': 'cors',
-    'Sec-Fetch-Dest': 'empty',
-    'Referer': 'https://gradaustralia.com.au/',
     'Accept-Language': 'en-US,en;q=0.9,ar;q=0.8,de;q=0.7',
+    'Connection': 'keep-alive',
+    'Origin': 'https://gradaustralia.com.au',
+    'Referer': 'https://gradaustralia.com.au/',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'cross-site',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+    'accept': '*/*',
+    'content-type': 'application/json',
+    'sec-ch-ua': '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"',
 }
 links = []
-date = datetime.today().strftime('%d-%m-%Y')
-page_num = 0
+num = 0
 while True:
-    params = (
-        ('queryId', 'bdbf3a43ca8c5ad87479743e28be1a0ec883709d:37'),
-        ('operationName', 'SearchCareerOpportunity'),
-        ('variables', f'{{"gid":["6"],"index_id":"anabranch_connect_index","condition_group":{{"groups":[{{"conditions":[{{"name":"gid","operator":"=","value":"6"}},{{"name":"node_type","operator":"=","value":"career_opportunity"}},{{"name":"published","operator":"=","value":"1"}},{{"name":"expired","operator":"<>","value":"1"}},{{"name":"inactive_organisation","operator":"<>","value":"1"}},{{"name":"inactive_content","operator":"<>","value":"1"}}]}},{{"conditions":[],"conjunction":"OR","tags":["facet:study_field_tids"]}},{{"conditions":[],"conjunction":"OR","tags":["facet:opportunity_types"]}},{{"conditions":[],"conjunction":"OR","tags":["facet:locations"]}},{{"conditions":[],"conjunction":"OR","tags":["facet:industry_sectors"]}}],"conjunction":"AND"}},"range":{{"offset":{page_num},"limit":8}},"sort":[{{"field":"field_sticky","value":"desc"}},{{"field":"field_weight","value":"asc"}},{{"field":"applications_close_date","value":"asc"}}],"facets":[{{"field":"study_field_tids","limit":0,"operator":"OR","missing":false,"min_count":1}},{{"field":"opportunity_types","limit":0,"operator":"OR","missing":false,"min_count":1}},{{"field":"locations","limit":0,"operator":"OR","missing":false,"min_count":1}},{{"field":"industry_sectors","limit":0,"operator":"OR","missing":false,"min_count":1}}]}}'),
-    )
-    response = requests.get('https://prosple-content-api.global.ssl.fastly.net/graphql', headers=headers, params=params)
-    nodes = response.json()['data']['searchAPISearch']['groupNodes']
+    params = {
+        'operationName': 'OpportunitiesSearch',
+        'variables': f'{{"parameters":{{"gid":"6","range":{{"offset":{num},"limit":20}},"sortBy":{{"criteria":"POPULARITY","direction":"DESC"}}}}}}',
+        'extensions': '{"persistedQuery":{"version":1,"sha256Hash":"d7f497655d4deedfcf0ffe14f865e9803130e5a0a519018eb935d57c210cc048"}}',
+    }
+
+    response = requests.get('https://prosple-gw.global.ssl.fastly.net/internal', params=params, headers=headers)
+    nodes = response.json()['data']['opportunitiesSearch']['opportunities']
     if len(nodes) == 0:
         break
     for node in nodes:
-        links.append(node['entityUrl']['path'])
+        link = node['detailPageURL']
+        links.append(link)
     print(len(set(links)),end=',')
-    page_num += 8
+    num += 20
 
 for link in links:
-    params = (
-        ('queryId', 'bdbf3a43ca8c5ad87479743e28be1a0ec883709d:51'),
-        ('operationName', 'GroupContentCareerOpportunity'),
-        ('variables', f'{{"path":"{link}","gid":["6"]}}'),
-    )
+    params = {
+        'queryId': '542f850b108aa27e8bf1227a8bba4be0f1d43b78:49',
+        'operationName': 'GroupContentCareerOpportunity',
+        'variables': f'{{"path":"/group/6{link}","gid":["6"]}}',
+    }
 
-    response = requests.get('https://prosple-content-api.global.ssl.fastly.net/graphql', headers=headers, params=params)
+    response = requests.get('https://prosple-content-api.global.ssl.fastly.net/graphql', params=params, headers=headers)
     data = response.json()['data']['content']['entity']
     title = data['entityIdOfGroupContent']['entity']['title']
     otype = data['entityIdOfGroupContent']['entity']['fieldOpportunityTypes'][0]['entity']['entityLabel']
@@ -83,7 +84,7 @@ for link in links:
         employees = data['entityIdOfGroupContent']['entity']['fieldParentEmployer']['entity']['fieldNumEmployees']['entity']['entityLabel']
     except:
         employees = ''
-    link = link.replace('/group/6','https://gradaustralia.com.au')
+    link = 'https://gradaustralia.com.au'+link
     if os.path.exists('gaus.csv'):
         newfile = False
     else:
